bc1dd2e6060524b98c6e29bcfc4ea02c0c5d749f
omarw <omar.wheatley@gmail.com>
2015-03-05 20:51:13 -0500

    sync existing values in text element
    sync existing values in text element when initializing.

--- javascripts/jquery.listarea.js
+++ javascripts/jquery.listarea.js
@@ -9,11 +9,13 @@
 ;(function($, window, undefined){
 	$.fn.listarea = function(params) {
 		params = $.extend( {delimiter: ',', effect: null, placeholder: "add an item"}, params);
+		var textArray = $(this).val().split(params.delimiter);		
 		var textareaInput = $(this).hide().after('<div class="addListItem"><input type="text" placeholder="'+params.placeholder+'"/><div class="addButton"></div></div>');
 		var listareaWrap = $('div.addListItem');
+		syncExistingValues(textArray, listareaWrap, params);
 		$('div.addListItem > .addButton').on('click', function(){
 			lbAddListItem(textareaInput, listareaWrap, params);
-		});
+		});		
 		$('div.addListItem').on('click', 'div.listItemWrap .deleteButton', function(){
 			lbDeleteListItem(textareaInput, listareaWrap, $(this), params);
 		});
@@ -29,26 +31,32 @@
 		});
 		return this;
    };
-})( jQuery, window );
+   function syncExistingValues(textArray, listareaWrap, params){
+		for (i = 0; i < textArray.length; i++) { 
+			if(textArray[i] != ''){
+				$('<div class="listItemWrap"><div class="listItem">' + textArray[i] + '</div><div class="deleteButton"></div></div>').hide().prependTo(listareaWrap).show(params.effect);
+			}
+		}
+   }
+   function lbAddListItem(textareaInput, listareaWrap, params){
+		var itemValue = $('div.addListItem > input').val();
+		if(itemValue != ''){
+			textareaInput.next('div.addListItem').find('input').val('');
+			$('<div class="listItemWrap"><div class="listItem">' + itemValue + '</div><div class="deleteButton"></div></div>').hide().prependTo(listareaWrap).show(params.effect);
+			lbListItemToTextArea(textareaInput, listareaWrap, params);
+		}
+	}
 
-function lbAddListItem(textareaInput, listareaWrap, params){
-	var itemValue = $('div.addListItem > input').val();
-	if(itemValue != ''){
-		textareaInput.next('div.addListItem').find('input').val('');
-		$('<div class="listItemWrap"><div class="listItem">' + itemValue + '</div><div class="deleteButton"></div></div>').hide().prependTo(listareaWrap).show(params.effect);
+	function lbDeleteListItem(textareaInput, listareaWrap, item, params){
+		item.parent().remove();
 		lbListItemToTextArea(textareaInput, listareaWrap, params);
 	}
-}
 
-function lbDeleteListItem(textareaInput, listareaWrap, item, params){
-	item.parent().remove();
-	lbListItemToTextArea(textareaInput, listareaWrap, params);
-}
-
-function lbListItemToTextArea(textareaInput, listareaWrap, params){
-	var items = [];
-	listareaWrap.find('div.listItem').each(function(){
-		items.push( $(this).text() );
-	});
-	textareaInput.val(items.join(params.delimiter));
-}
+	function lbListItemToTextArea(textareaInput, listareaWrap, params){
+		var items = [];
+		listareaWrap.find('div.listItem').each(function(){
+			items.push( $(this).text() );
+		});
+		textareaInput.val(items.join(params.delimiter));
+	}
+})( jQuery, window );
